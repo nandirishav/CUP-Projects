7463a5c0cade88d0018f2e7abad201bd1979782e
Rishav Nandi <rishavnandi06@gmail.com>
2021-08-29 12:07:05 +0530

    added: superhero search app
    Signed-off-by: Rishav Nandi <rishavnandi06@gmail.com>

---
+++ superhero-hunter/scripts/details.js
@@ -0,0 +1,38 @@
+const name = document.getElementById("superhero-name");
+const image = document.getElementById("superhero-img");
+const params = new URLSearchParams(window.location.search);
+var id = params.get("id");
+
+//Fetching Superhero details from Superhero API
+fetch(`https://superheroapi.com/api.php/1953802511419258/${id}`)
+  .then((res) => res.json())
+  .then((data) => showDetails(data))
+  .catch((err) => console.log(err));
+
+function showDetails(data) {
+  name.innerHTML = data.name;
+  image.src = data.image.url;
+  image.alt = "Image not found";
+
+  for (var i in data.powerstats) {
+    document.getElementById(i).innerHTML = data.powerstats[i] + "%";
+    //Power Meter Style Setting
+    document.getElementById(i).style.width = data.powerstats[i] + "%";
+  }
+
+  for (var i in data.appearance) {
+    document.getElementById(i).innerHTML = data.appearance[i];
+  }
+
+  for (var i in data.biography) {
+    document.getElementById(i).innerHTML = data.biography[i];
+  }
+
+  for (var i in data.work) {
+    document.getElementById(i).innerHTML = data.work[i];
+  }
+
+  for (var i in data.connections) {
+    document.getElementById(i).innerHTML = data.connections[i];
+  }
+}
---
+++ superhero-hunter/scripts/index.js
@@ -0,0 +1,69 @@
+const inputName = document.getElementById("inputName");
+const results = document.getElementById("results");
+
+// SuperHero Name Input
+inputName.onkeyup = function () {
+  var name = inputName.value;
+
+  if (name !== "") {
+    fetch(
+      "https://superheroapi.com/api.php/1953802511419258/search/" + name.trim()
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        createCard(data);
+      })
+      .catch((err) => console.log(err));
+  }
+};
+
+function createCard(data) {
+  if (data.response === "error") {
+    results.innerHTML =
+      '<div style="margin-top:50px; color:white;">No Results Found! Please try a different name!</div>';
+  } else {
+    results.innerHTML = null;
+
+    for (let i = 0; i < data.results.length && i < 10; i++) {
+      var card = document.createElement("div");
+      var cardImage = document.createElement("img");
+      var cardContainer = document.createElement("div");
+      var cardText = document.createElement("div");
+      var favButton = document.createElement("div");
+      var detailsButton = document.createElement("div");
+
+      card.classList.add("result-card");
+      cardImage.classList.add("result-card-image");
+      cardContainer.classList.add("result-card-container");
+      favButton.classList.add("favButton");
+      detailsButton.classList.add("detailsButton");
+
+      cardText.innerHTML = data.results[i].name;
+      cardContainer.appendChild(cardText);
+      cardImage.src = data.results[i].image.url;
+      card.appendChild(cardImage);
+
+      detailsButton.innerHTML = "Know my superpowers 💪";
+      cardContainer.appendChild(detailsButton);
+
+      favButton.innerHTML = "Add to My Favourites 🧡";
+      favButton.classList.add("bg-red");
+      cardContainer.appendChild(favButton);
+      card.appendChild(cardContainer);
+
+      detailsButton.setAttribute("superheroId", data.results[i].id);
+      detailsButton.setAttribute("divType", "details-btn");
+      results.appendChild(card);
+    }
+  }
+}
+
+results.onclick = function (event) {
+  var id = event.target.getAttribute("superheroId");
+  var div = event.target.getAttribute("divType");
+
+  if (div === "details-btn") {
+    if (id === null) return;
+    window.open("details.html?id=" + id, "_self");
+  }
+};
