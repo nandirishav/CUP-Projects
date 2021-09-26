d9ed5ad1c64ec513bf1191726645120b1d2f9de6
Rishav Nandi <rishavnandi06@gmail.com>
2021-09-26 17:10:25 +0530

    added: start script and altPort
    Signed-off-by: Rishav Nandi <rishavnandi06@gmail.com>

--- CUP-Assignment3/index.js
+++ CUP-Assignment3/index.js
@@ -2,29 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT;
+const altPort = 3000;
 const db = require("./config/mongoose");
-
-//middlewares
-// app.use((req, res, next) => {
-//   // logic to not allow any route handlers to execute if user is not logged in
-//   if (req.method === "GET") {
-//     res.send("GET requests are disabled");
-//   } else {
-//     next();
-//   }
-
-//   next();
-// });
-
-// for maintenance mode
-// app.use((req, res, next) => {
-//   res.status(503).send('Site under maintenance , Please come back later !');
-//   next();
-// });
 
 app.use(express.json());
 app.use("/", require("./routes"));
 
-app.listen(port, () => {
+app.listen(port || altPort, () => {
   console.log("Listening at port", port);
 });
